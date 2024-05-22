b29aee30011a5c19c147c29c562e09b935ec4569
joao.camacho <joao.camacho@visionspace.com>
2024-05-22 12:00:27 +0100

    all works

--- src/data_processing/testing_postgre.py
+++ src/data_processing/testing_postgre.py
@@ -1,11 +1,11 @@
 import psycopg2  # type: ignore
 import pandas as pd  # type: ignore
-# import pyarrow.parquet as pq
+from sqlalchemy import create_engine
 
 # Docker container details
 host = "localhost"
 port = 5433
-database = "database_example"
+database = "root"
 user = "root"
 password = "pass123"
 
@@ -23,43 +23,62 @@ df = df.astype(str)
 # Create a cursor object
 cur = conn.cursor()
 table_name = "table_example"
-table_creation = '''
-   CREATE TABLE table_example (
-       stf_id SERIAL PRIMARY KEY,
-       cmd_type TEXT NOT NULL,
-       pool_id TEXT NOT NULL,
-       sandbox_id TEXT NOT NULL,
-       timestamp_str TEXT NOT NULL,
-       hostname TEXT NOT NULL,
-       cmd TEXT NOT NULL,
-       ip TEXT NOT NULL,
-       username TEXT NOT NULL,
-       wd TEXT NOT NULL
-   )
-'''
-cur.execute(table_creation)
+# table_creation = '''
+#    CREATE TABLE table_example (
+#        cmd_type TEXT NOT NULL,
+#        pool_id TEXT NOT NULL,
+#        sandbox_id TEXT NOT NULL,
+#        timestamp_str TEXT NOT NULL,
+#        hostname TEXT NOT NULL,
+#        cmd TEXT NOT NULL,
+#        ip TEXT NOT NULL,
+#        username TEXT NOT NULL,
+#        wd TEXT NOT NULL
+#    )
+# '''
+# cur.execute(table_creation)
+
+# Create an SQLAlchemy engine
+engine = create_engine(
+    f"postgresql://{user}:{password}@{host}:{port}/{database}",
+)
+
+df = df.astype(str)
+df.drop_duplicates(subset=None)
+df.to_sql(table_name, engine, method='multi', if_exists="replace", index=False)
 
 conn.commit()
 cur.close()
 conn.close()
 
-# import sys
-# sys.exit(0)
+# Reconnecting to database
+conn = psycopg2.connect(host=host, database=database, user=user,
+                        password=password, port=port)
 
-# # Define the insert statement with placeholders for data
-# insert_stmt = f"""INSERT INTO {table_name} ({", ".join(df.columns)}) VALUES (%s, %s, ...)"""
 
-# # Convert pandas DataFrame to a list of tuples (one tuple per row)
-# data_tuples = df.to_records(index=False).tolist()
+cursor = conn.cursor()
+# drop table accounts
+postgreSQL_select_Query = "SELECT * FROM table_example"
+# Executing the query
+cursor.execute(postgreSQL_select_Query)
 
-# # Execute the insert statement with data tuples
-# cur.executemany(insert_stmt, data_tuples)
+print("\nSELECT * FROM table_example:")
+records = cursor.fetchall()
+print(records)
 
-# # Commit the changes
-# conn.commit()
 
-# # Close the cursor and connection
-# cur.close()
-# conn.close()
+# Reconnecting to database
+conn = psycopg2.connect(host=host, database=database, user=user,
+                        password=password, port=port)
 
-# print(f"Parquet data uploaded to table {table_name}")
+
+cursor = conn.cursor()
+# drop table accounts
+sql = '''DROP TABLE table_example '''
+# Executing the query
+cursor.execute(sql)
+print("\nTable dropped!")
+
+# Commit changes  and close connection to the database
+conn.commit()
+conn.close()
